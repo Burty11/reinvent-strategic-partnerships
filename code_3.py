a1ecadaf13c5a002d676468f77ff1801f06cc36a
Burty11 <142454463+Burty11@users.noreply.github.com>
2023-12-15 20:57:26 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,42 @@
-    return data
-if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-def main():
-        print(f"Random Number: {item}")
-    data = generate_random_data()
+if __name__ == "__main__":
 
-    main()
+
 def generate_random_data():
+    return data
 
-    for item in data:
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    main()
 
+def main():
 import random
+    for item in data:        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+
+    const data = [];
+}
+        console.log("Random Number: " + item);
+
+
+}
+    });
+function main() {
+    }
+function generateRandomData() {
+main();
+    data.forEach(item => {        for (int item : data) {
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+        }
+        }
+}
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+    }
+        int[] data = new int[10];
